b96f5892862a62b42f533b5e8f3f0cb828145cac
Samuele Castaldo <castaldo.samuele@gmail.com>
2025-02-25 21:12:38 +0100

    co

--- src/components/utenti.js
+++ src/components/utenti.js
@@ -1,7 +1,7 @@
 var s1="T5SnwhoqoLeAFlpVzAmCW4Pt3qm1"  //sono io
 var s6="9uXrTRBIasMGV1AEqhhfEI7CmRN2"  //Cesario
 var s7="MwUDv28kFlhWq5MmmNTfurSKRLE3"  //Hola
-var s2="bBOeGRw4vHPHnRxn8ccZast5haY2"   //Nando Liguori
+var s2="GSgY1obKiYRFV1r7IYCXIJQqBI83"   //Nando Liguori
 var s3= "6aFLomc8ZXV33ze0EAKgRDh9iQV2"  //Paola Liguori
 var s5="gKV9G0Q3B7gvHX3ISXJO3HwTCmT2" //Davide Liguori
 var d1= "qAXzcdGLh9VSIM2gZVlbIGuijzh2"  //dipendente1
@@ -12,12 +12,12 @@ var g1= "wcRL7pe4fOarFYJeDaWGCsTHKRk2"  //guidatore
 export let primary= "#152c51";
 export let rosso= "#D32F2F";
 
-export let supa= [s1, s6, s7]  //array per i supervisori
+export let supa= [s1, s2, s7]  //array per i supervisori
 
 export let guid= [g1]  //array per i guidatori
 
 export let dipen= [d1]  //array per i dipendenti
 
-export let tutti= [s1,d1,g1,s6, s7] //array per chi può visualizzare la tabella
+export let tutti= [s1,d1,g1,s6, s7, s2,] //array per chi può visualizzare la tabella
 
 
