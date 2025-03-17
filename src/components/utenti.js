292682221c5b92821982245e4815a1b3bac3f1f1
Samuele Castaldo <castaldo.samuele@gmail.com>
2025-03-17 22:21:34 +0100

    Update utenti component to add a new employee identifier and modify arrays for improved data handling

--- src/components/utenti.js
+++ src/components/utenti.js
@@ -5,6 +5,7 @@ var s2="GSgY1obKiYRFV1r7IYCXIJQqBI83"   //Nando Liguori
 var s3= "6aFLomc8ZXV33ze0EAKgRDh9iQV2"  //Paola Liguori
 var s5="gKV9G0Q3B7gvHX3ISXJO3HwTCmT2" //Davide Liguori
 var d1= "qAXzcdGLh9VSIM2gZVlbIGuijzh2"  //dipendente1
+var d2 = "NHfPOA6suTN3u8ojDw7UWKk6dsf1"
 var g1= "wcRL7pe4fOarFYJeDaWGCsTHKRk2"  //guidatore
 
 
@@ -16,8 +17,8 @@ export let supa= [s1, s2, s7]  //array per i supervisori
 
 export let guid= [g1]  //array per i guidatori
 
-export let dipen= [d1]  //array per i dipendenti
+export let dipen= [d1, d2]  //array per i dipendenti
 
-export let tutti= [s1,d1,g1,s6, s7, s2,] //array per chi può visualizzare la tabella
+export let tutti= [s1,d1, d2, g1,s6, s7, s2,] //array per chi può visualizzare la tabella
 
 
